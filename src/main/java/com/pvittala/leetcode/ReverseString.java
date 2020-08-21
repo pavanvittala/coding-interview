421af48202d715f23891909341f0452a640ca413
Pavan Vittala <Darkstar0167@gmail.com>
2020-08-21 13:27:35 -0400

    Implemented LeetCode ReverseString problem.

---
+++ src/main/java/com/pvittala/leetcode/ReverseString.java
@@ -0,0 +1,28 @@
+package com.pvittala.leetcode;
+
+/**
+ * LeetCode ReverseString problem.
+ * @author Pavan Vittala
+ */
+public class ReverseString {
+    /**
+     * Write a function that reverses a string. The input string is given as an array of characters char[].
+     * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+     * You may assume all the characters consist of printable ascii characters.
+     * @param word String to be reversed in-place.
+     */
+    public static void reverseString(final char[] word) {
+        int leftIndex = 0;
+        int rightIndex = word.length-1;
+
+        // The first condition (rightIndex > 0) ensures empty arrays and arrays of length 1 are not processed
+        // The second condition (rightIndex > leftIndex) ensures that both even and odd length arrays can be processed
+        while (rightIndex > 0 && rightIndex > leftIndex) {
+            char temp = word[rightIndex];
+            word[rightIndex] = word[leftIndex];
+            word[leftIndex] = temp;
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+}
---
+++ src/test/java/com/pvittala/leetcode/ReverseStringTest.java
@@ -0,0 +1,40 @@
+package com.pvittala.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+
+    @Test
+    void reverseEmptyString() {
+        char[] word = {};
+        ReverseString.reverseString(word);
+        char[] expected = {};
+        assertArrayEquals(expected, word);
+    }
+
+    @Test
+    void reverseLengthOneString() {
+        char[] word = {'p'};
+        ReverseString.reverseString(word);
+        char[] expected = {'p'};
+        assertArrayEquals(expected, word);
+    }
+
+    @Test
+    void reverseEvenString() {
+        char[] word = {'w', 'e', 'l', 'l'};
+        ReverseString.reverseString(word);
+        char[] expected = {'l', 'l', 'e', 'w'};
+        assertArrayEquals(expected, word);
+    }
+
+    @Test
+    void reverseOddString() {
+        char[] word = {'h', 'e', 'l', 'l', 'o'};
+        ReverseString.reverseString(word);
+        char[] expected = {'o', 'l', 'l', 'e', 'h'};
+        assertArrayEquals(expected, word);
+    }
+}
